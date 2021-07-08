325f7b64d113a8b4d0aeb546ce9266b237cdcd1b
Nikita <33836924+NikInspirer@users.noreply.github.com>
2021-07-08 22:55:55 +0300

    Add GoKart pawn with simple movement forward

---
+++ Source/KrazyKarts/Pawns/GoKart.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright © 2021 Nikita Kuznetsov. All Rights Reserved.
+
+#include "GoKart.h"
+
+AGoKart::AGoKart()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const FVector WorldOffset = Velocity * 100 * DeltaTime;
+	AddActorWorldOffset(WorldOffset);
+}
+
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Velocity = GetActorForwardVector() * Value * 20;
+}
---
+++ Source/KrazyKarts/Pawns/GoKart.h
@@ -0,0 +1,33 @@
+﻿// Copyright © 2021 Nikita Kuznetsov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KRAZYKARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AGoKart();
+
+	//~ BEGIN AActor Interface
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	//~ END AActor Interface
+
+protected:
+	//~ BEGIN AActor Interface
+	virtual void BeginPlay() override;
+	//~ END AActor Interface
+
+private:
+	/** Velocity of movement in 3D (m/s). */
+	FVector Velocity;
+
+	/** Move kart forward - changes velocity based on specified Value. */
+	void MoveForward(float Value);
+};
